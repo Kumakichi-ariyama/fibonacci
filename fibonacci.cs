f6b87550cec79a9f6198020dc381375ed05aad81
Kumakichi-ariyama <103029995+Kumakichi-ariyama@users.noreply.github.com>
2022-04-11 08:13:01 +0300

    Create fibonacci.cs

---
+++ fibonacci.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+int result = Fibonacci(10);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i < n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
+}
+
+Console.ReadKey();
